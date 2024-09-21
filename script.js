54a7d42906f2c7accbccd3beca981a4bee0d0b2c
Jason Phan <jsonphan@gmail.com>
2024-09-21 15:39:49 -0700

    Functionality for Editing and Deleting recipes are working.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let recipes = [];
-
+let current_recipe;
 
 function renderRecipeList() {
   const recipeListContainer = document.querySelector('.recipeList');
@@ -10,8 +10,8 @@ function renderRecipeList() {
     recipeDiv.classList.add('recipeItem');
     recipeDiv.innerHTML = `
       <h3>${recipe.title}</h3>
-      <button class="recipeButton">Edit</button>
-      <button class="recipeButton">Delete</button>
+      <button class="recipeButton" onclick="editRecipe(${index})">Edit</button>
+      <button class="recipeButton" onclick="deleteRecipe(${index})">Delete</button>
       <p>${recipe.content.replace(/\n/g, '<br>')}</p>
     `;
     recipeListContainer.appendChild(recipeDiv);
@@ -38,4 +38,35 @@ function clearInputFields() {
   document.getElementById('recipeText').value = '';
 }
 
+function deleteRecipe(index){
+  recipes.splice(index, 1);
+  renderRecipeList();
+}
+function editRecipe(index){
+  document.getElementById('recipeTitle').value = recipes[index].title;
+  document.getElementById('recipeText').value = recipes[index].content;
+  current_recipe = index;
+
+}
+
+function saveRecipe(){
+  if (current_recipe != null){
+    const title = document.getElementById('recipeTitle').value;
+    const content = document.getElementById('recipeText').value;
+    if (title && content){
+      recipes[current_recipe] = {title, content};
+      renderRecipeList();
+      clearInputFields();
+      current_recipe = null;
+    }
+    else{
+      alert('Please fill out both the title and recipe fields.')
+    }
+  }
+  else{
+    alert('You are saving a nonexistent recipe.')
+  }
+}
+
 document.getElementById("addButton").addEventListener("click", addRecipe);
+document.getElementById("saveButton").addEventListener("click", saveRecipe);
