9b8435a54187aa28edc8027ce9d61b8787fe2c41
Jason Phan <jsonphan@gmail.com>
2024-09-22 14:56:34 -0700

    Added a way to view recipes on a bigger modal. Changed UI's colors

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ function renderRecipeList() {
   recipes.forEach((recipe, index) => {
     const recipeDiv = document.createElement('div');
     recipeDiv.classList.add('recipeItem');
+    recipeDiv.id = 'recipeItem'
+    recipeDiv.onclick = "showRecipe(${index})"
     recipeDiv.innerHTML = `
       <h3>${recipe.title}</h3>
       <button class="recipeButton" onclick="editRecipe(${index})">Edit</button>
       <button class="recipeButton" onclick="deleteRecipe(${index})">Delete</button>
+
       <p>${recipe.content.replace(/\n/g, '<br>')}</p>
     `;
+    recipeDiv.addEventListener('click', function(){
+      showRecipe(index);
+    });
+
     recipeListContainer.appendChild(recipeDiv);
   });
 }
@@ -42,6 +49,7 @@ function deleteRecipe(index){
   recipes.splice(index, 1);
   renderRecipeList();
 }
+
 function editRecipe(index){
   document.getElementById('recipeTitle').value = recipes[index].title;
   document.getElementById('recipeText').value = recipes[index].content;
@@ -67,6 +75,28 @@ function saveRecipe(){
     alert('You are saving a nonexistent recipe.')
   }
 }
+
+function showRecipe(index){
+  const title = recipes[index].title;
+  const content = recipes[index].content;
+  const mymodal = document.getElementById('mymodal');
+
+  const modalContent = mymodal.querySelector('.modal-content')
+  modalContent.innerHTML = `
+    <span class="close">&times;</span>
+    <h2>${title}</h2>
+    <p>${content.replace(/\n/g, '<br>')}</p>
+  `;
+
+  mymodal.style.display = 'block';
+  mymodal.style.display = 'flex'
+  modalContent.style.display ='block';
+  const closeButton = modalContent.querySelector('.close');
+  closeButton.addEventListener('click', function () {
+    mymodal.style.display = 'none';
+  });
+}
+
 
 document.getElementById("addButton").addEventListener("click", addRecipe);
 document.getElementById("saveButton").addEventListener("click", saveRecipe);
